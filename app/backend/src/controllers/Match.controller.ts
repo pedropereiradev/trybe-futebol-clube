0cfaa99c06737bc74ac3f8c5b93f38bc9f8befde
Pedro Pereira <pedro.2108@hotmail.com>
2022-09-30 18:53:44 -0300

    feat: Match controller implemented

--- app/backend/src/controllers/Match.controller.ts
+++ app/backend/src/controllers/Match.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import IMatch from '../interfaces/Match.interface';
 import MatchService from '../services/Match.service';
 
 export default class MatchController {
@@ -16,5 +17,35 @@ export default class MatchController {
     const result = await this.matchService.findAll();
 
     return res.status(200).json(result);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { authorization } = req.headers;
+
+    const data = req.body as IMatch;
+
+    const result = await this.matchService.create(data, authorization || '');
+
+    if (result.message) {
+      return res.status(result.status).json({ message: result.message });
+    }
+
+    return res.status(result.status).json({ id: result.id, ...data });
+  }
+
+  public async updateInProgress(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    await this.matchService.updateInProgress(Number(id));
+
+    return res.status(200).json({ message: 'Finished' });
+  }
+
+  public async updateGoals(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    await this.matchService.updateGoals(Number(id), req.body);
+
+    return res.status(200).json(req.body);
   }
 }
