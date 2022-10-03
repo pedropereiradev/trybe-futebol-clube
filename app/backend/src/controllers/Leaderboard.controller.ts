3aa43fdb6e5876d3d9d4cb24ab337d1af12d90e8
Pedro Pereira <pedro.2108@hotmail.com>
2022-10-03 18:18:29 -0300

    feat: Leaderboard away controller implemented

--- app/backend/src/controllers/Leaderboard.controller.ts
+++ app/backend/src/controllers/Leaderboard.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
+import LeaderboardHomeService from '../services/LeaderboardHome.service';
 import LeaderboardService from '../services/Leaderboard.service';
+import LeaderboardAwayService from '../services/LeaderboardAway.service';
 
 export default class LeaderboardController {
-  constructor(private leaderboardService: LeaderboardService) { }
+  constructor(
+    private leaderboardService: LeaderboardService,
+    private leaderboardHomeService: LeaderboardHomeService,
+    private leaderboardAwayService: LeaderboardAwayService,
+  ) { }
 
   public async getHomeLeaderboard(req: Request, res: Response): Promise<Response> {
-    const result = await this.leaderboardService.leaderboardHome();
+    const result = await this.leaderboardHomeService.leaderboard();
+
+    return res.status(200).json(result);
+  }
+
+  public async getAwayLeaderboard(req: Request, res: Response): Promise<Response> {
+    const result = await this.leaderboardAwayService.leaderboard();
 
     return res.status(200).json(result);
   }
