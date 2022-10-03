a4b00025057c594592d85919aa5d572846b23d98
Pedro Pereira <pedro.2108@hotmail.com>
2022-10-03 18:18:43 -0300

    feat: Leaderboard away route implemented

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -1,11 +1,22 @@
 import { Router } from 'express';
+import LeaderboardHomeService from '../services/LeaderboardHome.service';
 import LeaderboardController from '../controllers/Leaderboard.controller';
 import LeaderboardService from '../services/Leaderboard.service';
+import LeaderboardAwayService from '../services/LeaderboardAway.service';
 
 const router = Router();
 
-const leaderboardController = new LeaderboardController(new LeaderboardService());
+const leaderboardService = new LeaderboardService();
+const leaderboardHomeService = new LeaderboardHomeService();
+const leaderboardAwayService = new LeaderboardAwayService();
+
+const leaderboardController = new LeaderboardController(
+  leaderboardService,
+  leaderboardHomeService,
+  leaderboardAwayService,
+);
 
 router.get('/home', (req, res) => leaderboardController.getHomeLeaderboard(req, res));
+router.get('/away', (req, res) => leaderboardController.getAwayLeaderboard(req, res));
 
 export default router;
