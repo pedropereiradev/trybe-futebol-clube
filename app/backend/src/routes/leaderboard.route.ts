5f6249eb27bb73d2e7c19b99635a25a2899380c7
Pedro Pereira <pedro.2108@hotmail.com>
2022-10-04 16:21:09 -0300

    feat: Full leaderboard route implemented

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -1,22 +1,26 @@
 import { Router } from 'express';
 import LeaderboardHomeService from '../services/LeaderboardHome.service';
 import LeaderboardController from '../controllers/Leaderboard.controller';
-import LeaderboardService from '../services/Leaderboard.service';
 import LeaderboardAwayService from '../services/LeaderboardAway.service';
+import LeaderboardCompleteService from '../services/LeaderboardComplete.service';
 
 const router = Router();
 
-const leaderboardService = new LeaderboardService();
 const leaderboardHomeService = new LeaderboardHomeService();
 const leaderboardAwayService = new LeaderboardAwayService();
-
-const leaderboardController = new LeaderboardController(
-  leaderboardService,
+const leaderboardCompleteService = new LeaderboardCompleteService(
   leaderboardHomeService,
   leaderboardAwayService,
 );
 
+const leaderboardController = new LeaderboardController(
+  leaderboardHomeService,
+  leaderboardAwayService,
+  leaderboardCompleteService,
+);
+
 router.get('/home', (req, res) => leaderboardController.getHomeLeaderboard(req, res));
 router.get('/away', (req, res) => leaderboardController.getAwayLeaderboard(req, res));
+router.get('/', (req, res) => leaderboardController.getCompleteLeaderboard(req, res));
 
 export default router;
